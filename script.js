11d6b8dd9544d172f41e81869497555f6480027c
olha25 <113186296+olha25@users.noreply.github.com>
2022-09-17 14:10:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function getNumberOrString(value) {
     return number_value
   }
 }
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
