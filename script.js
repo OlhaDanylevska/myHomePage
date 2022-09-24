d9e0422fe697d448dabbd59759258ddb8f8ceeab
olha25 <113186296+olha25@users.noreply.github.com>
2022-09-24 11:41:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+var array, images_next;
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+array = ['https://is2-ssl.mzstatic.com/image/thumb/Purple60/v4/24/cb/5b/24cb5b0c-3563-0d49-10be-54ce67ee7b25/source/256x256bb.jpg', 'https://b.thumbs.redditmedia.com/6qT0Layv6DvETeSGVGNORPzRweGgJZFJ8VJfQJW-xZk.png', 'https://sane-science.org/img/love-nature.jpg'];
+let element_picture = document.getElementById('picture');
+array.push(array[0]);
+element_picture.setAttribute("src", array.shift());
+
+
+document.getElementById('button_pre').addEventListener('click', (event) => {
+  let element_picture2 = document.getElementById('picture');
+  array.unshift(array.slice(-1)[0]);
+  element_picture2.setAttribute("src", array.pop());
+
+});
+
+document.getElementById('button_next').addEventListener('click', (event) => {
+  let element_picture3 = document.getElementById('picture');
+  array.push(array[0]);
+  element_picture3.setAttribute("src", array.shift());
+
+});
+randomMember(images_next);
+
+
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
   let number_value = Number(value);
